b89a1f3e639545ffc68eaadbc92b5acacb295f40
William Doane <wil@DrDoane.com>
2011-01-15 17:46:04 -0500

    Updated test language to reflect object oriented nature of code

--- stackOOP/tests/stackTest.js
+++ stackOOP/tests/stackTest.js
@@ -33,36 +33,36 @@ YUI().use('node', 'console', 'test', function(Y) {
     "test that function init is declared in the stack object" :
     function () {
       Y.assert( "function" === typeof(stack.init), "You need a function that will allow users of your code to initialize a new stack. This function must be in a stack object, named init, and defined in stack.js located in the src folder" );
-      Y.Assert.areEqual( 0, stack.init.length, "init must have no input parameter" );
+      Y.Assert.areEqual( 0, stack.init.length, "function init must have no input parameter" );
      },
 
     "test that function size is declared in the stack object" : 
     function () {
-      Y.assert( "function" === typeof(stack.size), "You need a function named stackSize that will allow users of your code to determine how many elements are in the stack" );
-      Y.Assert.areEqual( 0, stack.size.length, "stackSize must have no input parameter" );
+      Y.assert( "function" === typeof(stack.size), "You need a function named size that will allow users of your code to determine how many elements are in the stack" );
+      Y.Assert.areEqual( 0, stack.size.length, "function size must have no input parameter" );
     },
 
-    "test that stackSize returns a numeric value" : 
+    "test that size returns a numeric value" : 
     function () {
-      Y.Assert.isNumber( stack.size(), "stack.size() should return a number" );
+      Y.Assert.isNumber( stack.size(), "function size should return a number" );
     },
 
     "test that a new stack is of length 0" : 
     function () {
-      Y.Assert.areEqual( 0, stack.size(), "If no elements have been added to the stack, stack.size() should return 0" );
+      Y.Assert.areEqual( 0, stack.size(), "If no elements have been added to the stack, the size function should return 0" );
     },
 
     "test that function push is declared in the stack object" :
     function () {
-      Y.assert( "function" === typeof(stack.push), "You need a function named stack.push that allows users of your code to add elements to the stack" );
-      Y.Assert.areEqual( 1, stack.push.length, "stack.push must have one input parameter, the element to add" );
+      Y.assert( "function" === typeof(stack.push), "You need a function named push that allows users of your code to add elements to the stack" );
+      Y.Assert.areEqual( 1, stack.push.length, "function push must have one input parameter, the element to add" );
     },
 
     "test that a stack with 1 element is of length 1" :
     function () {
       stack.push("a");
 
-      Y.Assert.areEqual( 1, stack.size(), "If a stack has 1 element, stack.size() should return 1" );
+      Y.Assert.areEqual( 1, stack.size(), "If a stack has 1 element, function size should return 1" );
     },
 
     "test that a stack with 3 elements is of length 3" :
@@ -71,32 +71,32 @@ YUI().use('node', 'console', 'test', function(Y) {
       stack.push("b");
       stack.push("c");
 
-      Y.Assert.areEqual( 3, stack.size(), "If a stack has 3 elements, stack.size() should return 3" );
+      Y.Assert.areEqual( 3, stack.size(), "If a stack has 3 elements, function size should return 3" );
     },
 
     "test that function pop is declared in the stack object" :
     function () {
-      Y.assert( "function" === typeof(stack.pop), "You need a function named stack.pop that allows users of your code to get the last element added to the stack" );
-      Y.Assert.areEqual( 0, stack.pop.length, "stack.pop must have no input parameter" );
+      Y.assert( "function" === typeof(stack.pop), "You need a function named pop that allows users of your code to get the last element added to the stack" );
+      Y.Assert.areEqual( 0, stack.pop.length, "function pop must have no input parameter" );
     },
 
-    "test that pop returns the last element added" :
+    "test that function pop returns the last element added" :
     function () {
       stack.push("a");
       stack.push("b");
 
-      Y.Assert.areEqual( "b", stack.pop(), "Last element added should be returned by stack.pop()" );
+      Y.Assert.areEqual( "b", stack.pop(), "Last element added should be returned by function pop" );
     },
 
-    "test that pop removes the returned element from the stack" :
+    "test that function pop removes the returned element from the stack" :
     function () {
       stack.push("a");
       stack.push("b");
       stack.push("c");
 
-      Y.Assert.areEqual( "c", stack.pop(), "Last element added should be returned by stack.pop()" );
-      Y.Assert.areEqual( "b", stack.pop(), "next to last element added should be returned by a second stack.pop()" );
-      Y.Assert.areEqual( "a", stack.pop(), "first element added should be returned by a third stack.pop()" );
+      Y.Assert.areEqual( "c", stack.pop(), "Last element added should be returned by call to function pop" );
+      Y.Assert.areEqual( "b", stack.pop(), "next to last element added should be returned by a second call to function pop" );
+      Y.Assert.areEqual( "a", stack.pop(), "first element added should be returned by a third call to function pop" );
     },
 
     "test that stacks can contain 2 distinct elements with the same value" :
@@ -105,9 +105,9 @@ YUI().use('node', 'console', 'test', function(Y) {
       stack.push("b");
       stack.push("a");
 
-      Y.Assert.areEqual( "a", stack.pop(), "First call to stack.pop() should return last element added" );
-      Y.Assert.areEqual( "b", stack.pop(), "Second call to stack.pop() should return second to last element added" );
-      Y.Assert.areEqual( "a", stack.pop(), "Third call to stack.pop() should return third to last element added" );
+      Y.Assert.areEqual( "a", stack.pop(), "First call to function pop should return last element added" );
+      Y.Assert.areEqual( "b", stack.pop(), "Second call to function pop should return second to last element added" );
+      Y.Assert.areEqual( "a", stack.pop(), "Third call to function pop should return third to last element added" );
     },
 
     "test that stacks can contain string, number, boolean, and object elements and preserve data types" :
@@ -143,28 +143,28 @@ YUI().use('node', 'console', 'test', function(Y) {
 
     "test that function peek is declared in the stack object" :
     function () {
-      Y.assert( "function" === typeof(stack.peek), "You need a function named stack.peek that allows users of your code to non-destructively access any element in the stack" );
-      Y.Assert.areEqual( 1, stack.peek.length, "stack.peek must have one (optional) input parameter, the index of the element to be returned" );
+      Y.assert( "function" === typeof(stack.peek), "You need a function named peek that allows users of your code to non-destructively access any element in the stack" );
+      Y.Assert.areEqual( 1, stack.peek.length, "function peek must have one (optional) input parameter, the index of the element to be returned" );
     },
 
-    "test that stack.peek does not remove elements from the stack" :
+    "test that function peek does not remove elements from the stack" :
     function () {
       stack.push("a");
       stack.push("b");
       stack.push("c");
 
-      Y.Assert.areEqual( "c", stack.peek(), "stack.peek() should return the last element added without removing it from the stack" );
-      Y.Assert.areEqual( "c", stack.peek(), "stack.peek() should return the last element added without removing it from the stack" );
-      Y.Assert.areEqual( "c", stack.peek(), "stack.peek() should return the last element added without removing it from the stack" );
+      Y.Assert.areEqual( "c", stack.peek(), "function peek should return the last element added without removing it from the stack" );
+      Y.Assert.areEqual( "c", stack.peek(), "function peek should return the last element added without removing it from the stack" );
+      Y.Assert.areEqual( "c", stack.peek(), "function peek should return the last element added without removing it from the stack" );
     },
 
-    "test that stack.peek() and stack.peek(int) return undefined given an empty stack" :
+    "test that function peek returns undefined given an empty stack" :
     function () {
-      Y.Assert.areEqual( undefined, stack.peek(), "stack.peek() should return undefined for an empty stack" );
-      Y.Assert.areEqual( undefined, stack.peek(5), "stack.peek(int) should return undefined for an empty stack" );
+      Y.Assert.areEqual( undefined, stack.peek(), "a call to function peek with no arguments should return undefined for an empty stack" );
+      Y.Assert.areEqual( undefined, stack.peek(5), "a call to function peek with one number argument should return undefined for an empty stack" );
     },
 
-    "test that stack.peek allows random access to elements of the stack" :
+    "test that function peek allows random access to elements of the stack" :
     function () {
       stack.push("a");
       stack.push("b");
@@ -178,16 +178,16 @@ YUI().use('node', 'console', 'test', function(Y) {
 
     "test that function contains is declared in the stack object" :
     function () {
-      Y.assert( "function" === typeof(stack.contains), "You need a function named stack.contains that allows users of your code to determine whether a given element occurs in the stack" );
-      Y.Assert.areEqual( 1, stack.contains.length, "stack.contains must have one input parameter, the element for which to search" );
+      Y.assert( "function" === typeof(stack.contains), "You need a function named contains that allows users of your code to determine whether a given element occurs in the stack" );
+      Y.Assert.areEqual( 1, stack.contains.length, "function contains must have one input parameter, the element for which to search" );
     },
 
-    "test that stack.pop returns undefined given an empty stack" :
+    "test that function pop returns undefined given an empty stack" :
     function () {
-      Y.Assert.areEqual( undefined, stack.pop(), "stack.pop() should return undefined for an empty stack" );
+      Y.Assert.areEqual( undefined, stack.pop(), "function pop should return undefined for an empty stack" );
     },
 
-    "test that stack.contains returns false, if the stack is empty" :
+    "test that function contains returns false, if the stack is empty" :
     function () {
       Y.Assert.areEqual( false, stack.contains("a"), "'a' should not be in the stack" );
       Y.Assert.areEqual( false, stack.contains("b"), "'b' should not be in the stack" );
@@ -197,7 +197,7 @@ YUI().use('node', 'console', 'test', function(Y) {
       Y.Assert.areEqual( false, stack.contains(3), "3 should not be in the stack" );
     },
 
-    "test that stack.contains returns true, if the sought element is in the stack" :
+    "test that function contains returns true, if the sought element is in the stack" :
     function () {
       stack.push("a");
       stack.push("b");
@@ -214,7 +214,7 @@ YUI().use('node', 'console', 'test', function(Y) {
       Y.Assert.areEqual( true, stack.contains(3), "3 should be in the stack" );
     },
 
-    "test that stack.contains returns false, if the sought element is not in the stack" :
+    "test that function contains returns false, if the sought element is not in the stack" :
     function () {
       stack.push("a");
       stack.push("b");
