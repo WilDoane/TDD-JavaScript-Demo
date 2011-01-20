fc00fc762a7ba9a1cf14444d95f70d840b054d06
William Doane <wejdoane@williamdoane.local>
2011-01-20 08:02:18 -0500

    Updated to pass JSLint

--- template/tests/appTest.js
+++ template/tests/appTest.js
@@ -1,15 +1,30 @@
+/*global YUI, app, authorsName */
+/*jslint onevar: true, undef: true, newcap: true, nomen: true, regexp: true, plusplus: true, bitwise: true */
 // Create new YUI instance, and populate it with the required modules
 YUI().use('node', 'console', 'test', function(Y) {
- 
-  // Test available, and ready for use.
+  
+  var unitTests, reporter;
+  
+  //
+  // SUPPORT FUNCTIONS 
+  //
+  function authorsNameExists() {
+    return ( "string" === typeof(authorsName) ) ? true : false;
+  }
 
-  var unitTests = new Y.Test.Case({
+  function appInitExists() {
+    return ( "function" === typeof(app.init) ) ? true : false;
+  }
+
+  unitTests = new Y.Test.Case({
  
     name: "Testing app implementation",
 
     setUp :
     function () {
-      appInitExists() ? app.init() : null;
+      if ( appInitExists() ) {
+        app.init();
+      }
     },
 
     tearDown :
@@ -23,53 +38,46 @@ YUI().use('node', 'console', 'test', function(Y) {
 
     "test that the variable authorsName follows the proper format" :
     function () {
-      var re = /^[A-Za-z\'\-]+\s+[A-Za-z\'\-]+/;
+      var re, sourceString;
       
-      var sourceString = authorsNameExists() ? authorsName : "";
+      re = /^[A-Za-z\'\-]+\s+[A-Za-z\'\-]+/;
+      sourceString = authorsNameExists() ? authorsName : "";
       
       Y.Assert.isNotNull( sourceString.match(re), "The variable authorsName must have a value of 'Yourfirstname Yourlastname' (insert your own name)" );
     },
+
 
     "test that function init is declared in the stack object" :
     function () {
       Y.assert( "function" === typeof(app.init), "You need a function that will allow users of your code to initialize a new app. This function must be in a app object, named init, and defined in app.js located in the src folder" );
       Y.Assert.areEqual( 0, app.init.length, "function init must have no input parameter" );
-     },
+     }
 
 
   });
 
+  function logResultsToServer(data){
+    var testResults, serverReporter;
 
-  var r = new Y.Console({
+    testResults = Y.Test.Runner.getResults();
+  
+    serverReporter = new Y.Test.Reporter("http://nwghost.com/tdd-collector.php", Y.Test.Format.TAP);
+  
+    serverReporter.addField("authorsName", authorsNameExists() ? authorsName : "UNKNOWN AUTHOR");
+    
+    serverReporter.report(testResults);
+  }
+
+  reporter = new Y.Console({
       newestOnTop: false
   });  
 
-  r.render("#testReport");
+  reporter.render("#testReport");
   
   Y.Test.Runner.add(unitTests);
 
   Y.Test.Runner.subscribe(Y.Test.Runner.COMPLETE_EVENT, logResultsToServer);
   
   Y.Test.Runner.run();
-  
-  function logResultsToServer(data){
-    //get results
-    var testResults = Y.Test.Runner.getResults();
-  
-    var reporter = new Y.Test.Reporter("http://nwghost.com/tdd-collector.php", Y.Test.Format.TAP);
-  
-    reporter.addField("authorsName", authorsNameExists() ? authorsName : "UNKNOWN AUTHOR");
-    
-    reporter.report(testResults);
-    //reporter.destroy();
-  }
-  
-  function authorsNameExists() {
-    return ( "string" === typeof(authorsName) ) ? true : false;
-  }
-
-  function appInitExists() {
-    return ( "function" === typeof(app.init) ) ? true : false;
-  }
-  
+      
  });
